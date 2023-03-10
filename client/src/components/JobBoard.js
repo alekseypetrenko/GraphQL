88094445e19f7fc6430927db5c140de860dcacca
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-03-10 17:11:25 +0200

    added server and client

---
+++ client/src/components/JobBoard.js
@@ -0,0 +1,18 @@
+import JobList from "./JobList";
+import { getJobs } from "../graphql/queries";
+import { useState, useEffect } from "react";
+
+function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then(setJobs);
+  }, []);
+  return (
+    <div>
+      <h1 className="title">Job Board</h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+}
+
+export default JobBoard;
