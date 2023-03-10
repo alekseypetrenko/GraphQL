da5cc9e86c0bd7156c1c380c0242282a109b07f1
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-03-10 21:22:33 +0200

    add Job type, query it and render

--- client/src/components/JobDetail.js
+++ client/src/components/JobDetail.js
@@ -1,24 +1,25 @@
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
-import { jobs } from '../fake-data';
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { getJob } from "../graphql/queries";
+import { useEffect, useState } from "react";
 
 function JobDetail() {
   const { jobId } = useParams();
+  const [job, setJob] = useState(null);
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId]);
 
-  const job = jobs.find((job) => job.id === jobId);
+  if (!job) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
-      <h1 className="title">
-        {job.title}
-      </h1>
+      <h1 className="title">{job.title}</h1>
       <h2 className="subtitle">
-        <Link to={`/companies/${job.company.id}`}>
-          {job.company.name}
-        </Link>
+        <Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
       </h2>
-      <div className="box">
-        {job.description}
-      </div>
+      <div className="box">{job.description}</div>
     </div>
   );
 }
