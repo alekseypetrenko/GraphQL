a23fa48ad8ccd9f43fa74c2f232398f5bacd6be9
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-03-17 20:18:48 +0200

    added error handling

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -4,9 +4,16 @@ import { useState, useEffect } from "react";
 
 function JobBoard() {
   const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState(false);
   useEffect(() => {
-    getJobs().then(setJobs);
+    getJobs()
+      .then(setJobs)
+      .catch((err) => setError(true));
   }, []);
+
+  if (error) {
+    return <p>Something went wrong</p>;
+  }
   return (
     <div>
       <h1 className="title">Job Board</h1>
